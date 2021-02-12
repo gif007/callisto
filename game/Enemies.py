4fed976d7e056fa3ae3633829ca22d0428adc881
bard <carruthers.n.t@gmail.com>
2021-02-12 12:11:49 -0400

    added templates for some basic enemies that can be encountered in the starting area

--- game/Enemies.py
+++ game/Enemies.py
@@ -13,13 +13,47 @@
 gelatinous_blob = {
     'name': 'Gelatinous Blob',
     'desc': 'A slimey creature with no fixed shape',
+    'max_hp': 150,
+    'current_hp': 150,
+    'firepower': 2,
+    'firerate': 0.5,
+    'armor': 5,
+}
+
+ice_spider = {
+    'name': 'Ice Spider',
+    'desc': 'An arachnid-like organism that is completely incased in ice',
+    'max_hp': 75,
+    'current_hp': 75,
+    'firepower': 5,
+    'firerate': 1.7,
+    'armor': 30,
+}
+
+rock_spider = {
+    'name': 'Rock Spider',
+    'desc': 'An arachnid-like organism that is completely made of rock',
+    'max_hp': 75,
+    'current_hp': 75,
+    'firepower': 5,
+    'firerate': 2.2,
+    'armor': 20,
+}
+
+eel = {
+    'name': 'Eel',
+    'desc': 'A saltwater eel from the oceanic depths below the surface',
     'max_hp': 100,
     'current_hp': 100,
-    'firepower': 2,
-    'firerate': 0.5
-    'armor': 15
+    'firepower': 15,
+    'firerate': 1,
+    'armor': 10,
 }
 
 ENEMIES = [
     gelatinous_blob,
+    ice_spider,
+    rock_spider,
+    eel,
+
 ]
